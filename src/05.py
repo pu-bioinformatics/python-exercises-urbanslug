052bac98c0726621c24c7b3e5fd423878dd0aa7c
Njagi Mwaniki <njagi@urbanslug.com>
2019-03-12 12:39:32 +0300

    Add exercise 05 on loops

---
+++ src/05.py
@@ -0,0 +1,19 @@
+# Create a while loop that starts with x = 0 and increments
+# x until x is equal to 5. Each iteration should print to the console.
+print("Question 1")
+x = 0
+while x < 6:
+    print("x = %d" % x)
+    x = x+1
+
+# Repeat the previous problem, but the loop will skip printing
+# x = 5 to the console but will print values of x from 6 to 10.
+print("\nQuestion 2")
+while x < 10:
+    print("x = %d" % x)
+    x = x+1
+
+# Create a for loop that prints values from 4 to 10 to the console.
+print("\nQuestion 3")
+for i in range(4,11):
+    print("x = %d" % x)
